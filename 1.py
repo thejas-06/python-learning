e8e34e87e3a029bbfdb1e85cb1e8c3932c542a44
Thejas AN <tejasanteju@gmail.com>
2025-08-10 06:14:29 +0000

    practice

---
+++ 1.py
@@ -0,0 +1,13 @@
+a = "10" # a is a variable of type string
+b = 5.0
+A = int(a) # example for type conversion
+B = int(b)
+
+# we can assign variables like this also a,b = 10,5
+
+print(A + B)
+print(abs(B - A))
+print(A * B)
+print(A / B)
+print(A // B)
+print(A ** B) #Exponential
