dc3093299801495e3052f4ca7265d56a132ce79e
Thejas AN <tejasanteju@gmail.com>
2025-08-13 13:25:34 +0000

    practice

--- practice/3.py
+++ practice/3.py
@@ -6,7 +6,6 @@ girl_age = int(input("enter the girl age: "))
 
 # here i use abs because sometime boy might be younger 
 age_difference = abs(boy_age - girl_age)
-
 print(boy_name + " loves " + girl_name + " and the age difference is " + str(age_difference) )  # String concatenation 
 
 print(f"{boy_name} loves {girl_name} and the age difference is {age_difference}")  #string formate
---
+++ practice/4.py
@@ -0,0 +1,10 @@
+print("who are you ")
+print((" i am a robot\n")*2) # repetition 
+
+# string methods
+
+message = " i am a robot "
+print(message.upper()) # converts to capital letter  
+print(message.lower()) # converts to lowercase
+print(message.strip()) # Remove spaces from a string
+print(message.replace("a " , "not ")) # It replaces older string by new string
