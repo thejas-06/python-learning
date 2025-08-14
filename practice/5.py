77a2409071ce9870da6464b6fb13c991e99a56c6
Thejas AN <tejasanteju@gmail.com>
2025-08-14 17:52:29 +0000

    string

--- practice/5.py
+++ practice/5.py
@@ -1,6 +1,12 @@
-#simple greeting program using the + operator and f-srting for output
+#simple greeting program using the + operator and f-string for output
 name = input("enter your name:")
 age = input("enter your age:")
 print(f"'hello!' {name} i can not believe your {age}")
 print("'hello!' " + name + " i can not believe your " + age) 
 
+#string manipulation exercise
+user = input("what your doing:")
+print(user.upper())
+print(user.lower())
+print(user.split())
+print(user.replace(" " , "_"))
