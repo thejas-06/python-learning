cade68769f6037356fc15156f51fd5952bffd63f
Thejas AN <tejasanteju@gmail.com>
2025-08-14 17:11:05 +0000

    string

---
+++ practice/5.py
@@ -0,0 +1,6 @@
+#simple greeting program using the + operator and f-srting for output
+name = input("enter your name:")
+age = input("enter your age:")
+print(f"'hello!' {name} i can not believe your {age}")
+print("'hello!' " + name + " i can not believe your " + age) 
+
