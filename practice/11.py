9b384e309721e07bb39f2dbd785dac9520445a99
Thejas AN <tejasanteju@gmail.com>
2025-08-17 10:52:09 +0000

    set

---
+++ practice/11.py
@@ -0,0 +1,30 @@
+#sets
+number = {1,} #single set
+print(number)
+
+empty_set = set() #empty set do not leave like set = {}
+
+#set operations
+set1 = {1,2,3}
+set2 = {3,4,5}
+print(set1 | set2) #union operation
+print(set1 & set2) #intersection operation
+print(set1 - set2) #difference
+
+#set meyhods
+set = {"apple", "orange"}
+print(set)
+
+set.add("thejas") #add at random position
+print(set)
+
+set.remove("thejas")
+print(set)
+
+set.discard("teju") # set.remove("teju") this comes typeerror when we try to delete what not in set
+print(set)
+
+print(set.pop()) # it pop random element
+
+set.clear()
+print(set)
