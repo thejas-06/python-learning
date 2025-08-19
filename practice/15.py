4544a86a41b552692c4805cf86212b4ddc2ddf2c
Thejas AN <tejasanteju@gmail.com>
2025-08-19 16:55:50 +0000

    else

---
+++ practice/15.py
@@ -0,0 +1,8 @@
+age = int(input("enter your age:"))
+if age<18:
+    print("you are eligible for student membership")
+elif age>60:
+    print("you are eligible for senior citizenship") 
+else :
+    print("regular plan")
+           
