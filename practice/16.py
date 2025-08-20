6136bdb8e6be996d4be53ac51638cf8b8e3540de
Thejas AN <tejasanteju@gmail.com>
2025-08-20 14:28:26 +0000

    while

--- practice/16.py
+++ practice/16.py
@@ -7,3 +7,15 @@ while i<=10:
         x += 1
     print("")     
     i += 1    
+
+
+db = "1010"
+trail  = 1
+while trail<=3:
+    pin =(input(f"trail {trail} | pin-->"))
+    trail+=1
+    if pin == db:
+        print("correct")
+        break
+    else:
+        print("incorrect")    
