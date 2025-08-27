d9788653e4080b098cd75b1876f30aa0c5786a34
Thejas AN <tejasanteju@gmail.com>
2025-08-27 16:57:57 +0000

    for

---
+++ practice/18.py
@@ -0,0 +1,24 @@
+#sum using for loop
+num = [4,2,76,234,236,875]
+total = 0
+for i in num:
+    total = total + i
+print(total)
+
+#doubling a number 
+num = [4,2,76,234,236,875]
+dob = []
+for i in num:
+    dob.append(i*2)
+print(dob)    
+
+
+# Loops in dictionary 
+students = {
+    "thejas":58,
+    "dushyant":19,
+    "ragesh":41
+}
+
+for name, usn in students.items():  #we can also use .values() when we don't want keys
+    print(f"{name} - {usn}")   
